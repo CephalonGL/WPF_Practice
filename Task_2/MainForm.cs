b5c71d7730a57e3072a49264da7ef9ce7ee91329
Gregory Lubov <glubov@yandex.ru>
2022-11-26 22:32:45 +0700

    feature(Task_2_WPF): add prject.

---
+++ Task_2/MainForm.cs
@@ -0,0 +1,15 @@
+namespace Task_2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
