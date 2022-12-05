ea9bf1f0197e189ec3dd02d91cca600e41eeb216
Gregory Lubov <glubov@yandex.ru>
2022-12-06 01:05:12 +0700

    feature(Task_3): add View, ViewModel and Model projects.

---
+++ Task_3_Model/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Task_3_Model
+{
+    public class Class1
+    {
+    }
+}
---
+++ Task_3_ViewModel/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Task_3_ViewModel
+{
+    public class Class1
+    {
+    }
+}
